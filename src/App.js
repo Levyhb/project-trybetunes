6a531590e874f3aed2943d2f9965ec4c421bed56
Levy Bezerra <levybholanda@gmail.com>
2022-08-01 19:10:43 -0300

    Requisito 02 concluído

--- src/App.js
+++ src/App.js
@@ -9,24 +9,62 @@ import ProfileEdit from './pages/ProfileEdit';
 import Search from './pages/Search';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      isButtonDisabled: true,
+      isLoading: false,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ username: target.value }, this.handleButton);
+  }
+
+  handleButton = () => {
+    const { username } = this.state;
+    const maxLength = 3;
+    const enabledButton = username.length < maxLength;
+    this.setState({ isButtonDisabled: enabledButton });
+  }
+
+  loadingEvent = (loadingState) => {
+    this.setState({ isLoading: loadingState });
+  }
+
   render() {
+    const { username, isButtonDisabled, isLoading } = this.state;
     return (
       <div>
         <h1> TrybeTunes</h1>
 
         <Switch>
-          <Route  path="/album/:id" component={ Album } />
-          <Route  path="/profile/edit" component={ ProfileEdit } />
-          <Route  path="/profile" component={ Profile } />
-          <Route  path="/favorites" component={ Favorites } />
-          <Route  path="/search" component={ Search } />
-          <Route exact path="/" component={ Login } />
-          <Route component={ NotFound }/>
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/search" component={ Search } />
+          <Route
+            exact
+            path="/"
+            render={ (props) => (
+              <Login
+                { ...props }
+                handleChange={ this.handleChange }
+                username={ username }
+                isButtonDisabled={ isButtonDisabled }
+                isLoading={ isLoading }
+                loadingEvent={ this.loadingEvent }
+              />
+            ) }
+          />
+          <Route component={ NotFound } />
         </Switch>
       </div>
 
-    )
-    
+    );
   }
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,55 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends Component {
+  handleCreateUser = async () => {
+    const { username, loadingEvent, history } = this.props;
+    loadingEvent(true);
+    await createUser({ name: username });
+    history.push('/search');
+  }
+
   render() {
+    const { isButtonDisabled, handleChange, isLoading } = this.props;
+
     return (
       <div data-testid="page-login">
-        
+        { isLoading
+          ? <Loading />
+          : (
+            <form action="">
+              <label htmlFor="login-name">
+                <input
+                  type="text"
+                  data-testid="login-name-input"
+                  id="login-name"
+                  onChange={ handleChange }
+                />
+              </label>
+
+              <button
+                data-testid="login-submit-button"
+                onClick={ this.handleCreateUser }
+                type="button"
+                disabled={ isButtonDisabled }
+              >
+                Entrar
+              </button>
+            </form>) }
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  isButtonDisabled: PropTypes.bool.isRequired,
+  handleChange: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  history: PropTypes.string.isRequired,
+  loadingEvent: PropTypes.bool.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+};
 
 export default Login;
